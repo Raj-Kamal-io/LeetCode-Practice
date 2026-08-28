cc163f80f1a06f1498794dc3f7dbf7f0ffccabee
Raj kamal <146917824+Raj-Kamal-io@users.noreply.github.com>
2026-08-29 02:16:24 +0530

    Time: 1 ms (92.57%) | Memory: 41.7 MB (99.43%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.java
+++ 900-reordered-power-of-2/reordered-power-of-2.java
@@ -1,27 +1,21 @@
 class Solution {
     public boolean reorderedPowerOf2(int n) {
         int [] mp = new int[10];
-
         while(n> 0){
             mp[n % 10]++;
             n /= 10;
         }
-
         for(int i = 0; i <31; i++){
             int res = 1 << i;
-
-            int [] mp1 = new  int[10];
-
+            int[]mp1 = new  int[10];
             while(res > 0){
                 mp1[res % 10]++;
                 res /= 10;
             }
-
             if (Arrays.equals(mp , mp1)){
                 return true;
             }
         }
-
         return false;
     }
 }
