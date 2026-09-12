30ebe237aeac67fd1212127c2a593fee20d035ce
Raj kamal <146917824+Raj-Kamal-io@users.noreply.github.com>
2026-09-13 02:41:16 +0530

    Time: 18 ms (93.21%) | Memory: 26.1 MB (89.20%) - LeetSync

---
+++ 1543-simplified-fractions/simplified-fractions.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> simplifiedFractions(int n) {
+        vector<string> ans;
+        for(int i=1;i<n;i++)
+        {
+            for(int j=i+1;j<=n;j++)
+            {
+                if(gcd(i,j)==1)
+                {
+                   string temp=to_string(i)+'/'+to_string(j);
+                    ans.push_back(temp);
+                }
+            }
+        }
+        return ans;
+    }
+};
